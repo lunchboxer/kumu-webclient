b6d5eef9ae78182a00b9ea9c36f9ec8950fe047b
James <metal.lunchbox@gmail.com>
2019-04-04 20:58:09 +0800

    use dl for profile info

--- src/components/profile/Profile.svelte
+++ src/components/profile/Profile.svelte
@@ -9,20 +9,35 @@
 </script>
 
 <style>
-  li span.list-label {
-    display: inline-block;
-    width: 8rem;
-    font-weight: bold;
+  dl {
+    display: grid;
+    grid-column-gap: 0.5rem;
+    grid-row-gap: 0.5rem;
+    grid-template-columns: [key] auto [value] auto;
+    grid-template-rows: auto;
+    justify-content: start;
+  }
+
+  dt {
+    grid-column: key;
+    justify-self: end;
+  }
+
+  dd {
+    grid-column: value;
   }
 </style>
 
 <h1 class="title">User Profile</h1>
 
 {#if $me}
-<ul>
-  <li><span class="list-label">Username:</span> {$me.username}</li>
-  <li><span class="list-label">ID:</span> {$me.id}</li>
-</ul>
+<dl>
+  <dt>Username:</dt>
+  <dd>{$me.username}</dd>
+
+  <dt>ID:</dt>
+  <dd>{$me.id}</dd>
+</dl>
   {:else}
   <Loading what="user" />
 {/if}
