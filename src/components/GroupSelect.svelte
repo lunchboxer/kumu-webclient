d9da26723c13b3b50e17b4476563ccadf85c7c1c
James <metal.lunchbox@gmail.com>
2019-04-16 16:07:33 +0800

    fix bug, don't break when no classes in system

--- src/components/GroupSelect.svelte
+++ src/components/GroupSelect.svelte
@@ -3,21 +3,26 @@
   import { request } from '../data/fetch-client'
   import { ACTIVE_GROUPS } from '../data/queries'
 
-  export let groups
+  export let groups = null
   let selectElement
-  let error = ''
+  let errors = ''
   export let groupId = ''
   export let required = false
 
   onMount(async () => {
     if (!groups) {
-      const result = await request(ACTIVE_GROUPS)
-      groups = !result ? [] : result.activeGroups
-    }
+      try {
+        const result = await request(ACTIVE_GROUPS)
+        groups = !result ? [] : result.activeGroups
+      } catch (error) {
+        errors = error
+      }
+
+   }
   })
 
   function checkValidity () {
-    error = !selectElement.validity.valid && selectElement.validationMessage
+    errors = !selectElement.validity.valid && selectElement.validationMessage
       ? selectElement.validationMessage
       : ''
   }
@@ -30,21 +35,24 @@
 </style>
 
 <div class="field">
-
   <div class="select">
     <select bind:value={groupId} bind:this={selectElement} class:is-loading={!groups}
       on:invalid|preventDefault={checkValidity} {required}>
       {#if groups}
+      {#if groups.length}
       <option value="">Select group...</option>
       {#each groups as group}
         <option value={group.id}>{group.semester ? group.semester.name + ' ': ""}{group.name} class</option>
       {/each}
+      {:else}
+      <option>No active groups</option>
+      {/if}
     {:else}
       <option>Loading groups ...</option>
     {/if}
   </select>
 </div>
-{#if error} 
-<p class="help is-danger">{error}</p>
+{#if errors} 
+<p class="help is-danger">{JSON.stringify(errors)}</p>
 {/if}
 </div>
